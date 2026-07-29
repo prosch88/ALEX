e2d5f8d89f6952138164242179b66f600b5c982f
prosch88 <prosch88@googlemail.com>
2026-07-29 08:23:14 +0200

    modified:   alex.py

--- alex.py
+++ alex.py
@@ -62,6 +62,8 @@ from tkinter import StringVar
 if os.name == "posix":
     from alex.linux_dpi import get_linux_scale_factor
     scaling = get_linux_scale_factor()
+    if scaling == 1.5:
+        scaling += 0.04
 else:
     scaling = 1.0
 
